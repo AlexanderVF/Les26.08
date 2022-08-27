b6c13807e7e07820757cc1076ffb00b07f57c998
«alexander <favorite@bk.ru>
2022-08-27 14:40:43 +0300

    hw3

--- HW_3/Program.cs
+++ HW_3/Program.cs
@@ -3,16 +3,17 @@ Console.WriteLine("Введите число");
 int a = int.Parse(Console.ReadLine());
 
 int i = 1;
-
+Console.Write(a + " -> ");
 if (a < 0)
 {
     a = (a * (-1));
 }
 if (a > 0)
 {
-    while (a >= i)
+        while (a >= i)
     {
-        Console.WriteLine(Math.Pow(i, 2));
+        Console.Write(Math.Pow(i, 3) + ", ");
         i++;
     }
 }
+
