0ebcbab5b3838a0c95bea97ba131e825762e9b56
«alexander <favorite@bk.ru>
2022-08-29 12:20:39 +0300

    hw3_1

--- HW_3/Program.cs
+++ HW_3/Program.cs
@@ -4,10 +4,29 @@ int a = int.Parse(Console.ReadLine());
 
 int i = 1;
 Console.Write(a + " -> ");
+/* if (a < 0)
+{
+    a = (a * (-1));
+} */
+/* if (a < 0)
+{
+    while (a <= -2)
+    {
+    i = i * (-1);
+   Console.Write(Math.Pow(i, 3) + ", ");
+        i--;
+    } */
+    
 if (a < 0)
 {
     a = (a * (-1));
+    while (a >= i)
+    {
+        Console.Write(Math.Pow(i, 3) * (-1) + ", ");
+        i++;
+    };
 }
+
 if (a > 0)
 {
         while (a >= i)
